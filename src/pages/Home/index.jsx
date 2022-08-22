110410cf937afe6ee67c276b97f0e7424299ff03
Diego Pereira <diego@develop.diego.develop.com.br>
2022-08-22 20:11:07 -0300

    teste

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,53 @@
+import './styles.css';
+import axios from 'axios';
+import * as React from 'react';
+
+export default function Home() {
+
+    const [pokemons, setPokemons] = React.useState([]);
+    const [pokemonsFilters, setPokemonsFilters] = React.useState([]);
+    const [input, setInput] = React.useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setPokemonsFilters(pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(input.toLowerCase())));
+    }
+
+    React.useEffect(() => {
+        const getPokemons = async () => {
+            const response = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+            setPokemons(response.data.results);
+            setPokemonsFilters(response.data.results);
+        }
+        getPokemons();
+    }, [])
+
+    return (
+        <div className="container">
+            <h1 className="title">Pokédex</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    className="search"
+                    type="text"
+                    placeholder="Pesquisar..."
+                    onChange={(e) => setInput(e.target.value)}
+                    value={input}
+                />
+            </form>
+            <ul className='list'>
+                {
+                    pokemonsFilters.map((item, index) => (
+                        <li className='items'>
+                            <img
+                                className='image'
+                                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index + 1}.png`}
+                                alt={item.name}
+                            />
+                            <p className='name'>{item.name}</p>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
